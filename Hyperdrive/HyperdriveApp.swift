a9a209449049bcad57a9f565390640f38e567337
Alvin-Ta <86690210+Alvin-Ta@users.noreply.github.com>
2025-07-08 22:45:30 -0400

    Initial Commit

---
+++ Hyperdrive/HyperdriveApp.swift
@@ -0,0 +1,17 @@
+//
+//  HyperdriveApp.swift
+//  Hyperdrive
+//
+//  Created by Alvin Ta on 2025-07-08.
+//
+
+import SwiftUI
+
+@main
+struct HyperdriveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ HyperdriveTests/HyperdriveTests.swift
@@ -0,0 +1,17 @@
+//
+//  HyperdriveTests.swift
+//  HyperdriveTests
+//
+//  Created by Alvin Ta on 2025-07-08.
+//
+
+import Testing
+@testable import Hyperdrive
+
+struct HyperdriveTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
